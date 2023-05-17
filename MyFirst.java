72903e5a03fbc1f6ae97aa0dacf466dd7ad86dcd
Ashutosh Mishra <114138083+ashut123@users.noreply.github.com>
2023-05-17 22:44:59 +0530

    Add files via upload

---
+++ MyFirst.java
@@ -0,0 +1,8 @@
+import java.lang.*;
+ public class MyFirst {
+    public static void main(String arg[])  // here public is used so file name will be same as class name
+    {
+        System.out.println("Hello world");
+        
+    }
+}
