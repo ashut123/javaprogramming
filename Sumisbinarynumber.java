3d70fd84c3a245d37199b9aae35d41eb92760ab5
Ashutosh Mishra <114138083+ashut123@users.noreply.github.com>
2023-05-18 23:10:44 +0530

    Add files via upload

---
+++ Sumisbinarynumber.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+public class Sumisbinarynumber {
+    public static void main(String arg[])
+    {
+        Scanner sc=new Scanner(System.in);
+
+        sc.useRadix(2);
+        int x=sc.nextInt();
+        System.out.println(x);
+    }
+    
+}
