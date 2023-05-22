c83300319bbec39b3cdbd6f413106a4c3b237ee4
Ashutosh Mishra <114138083+ashut123@users.noreply.github.com>
2023-05-23 00:03:12 +0530

    Add files via upload

---
+++ Datasizerange2.java
@@ -0,0 +1,11 @@
+public class Datasizerange2 {
+    public static void main(String arg[])
+    {
+        System.out.println("Byte Min:"+  Byte.MIN_VALUE);
+        System.out.println("Byte Max:"+ Byte.MAX_VALUE);
+        System.out.println("Byte Bytes "+Byte.BYTES);
+
+    }
+
+    
+}
