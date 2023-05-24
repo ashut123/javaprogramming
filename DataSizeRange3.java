1681753786ba0192868c252b4cc52c1ccad9b2ad
Ashutosh Mishra <114138083+ashut123@users.noreply.github.com>
2023-05-24 22:29:20 +0530

    Add files via upload

---
+++ DataSizeRange3.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+public class DataSizeRange3 {
+    public static void main(String arg[])
+    {
+        System.out.println("Float Min; "+Float.MIN_VALUE);
+        System.out.println("Float Max; "+Float.MAX_VALUE);
+        System.out.println("Float Bytes " +Float.BYTES);
+    }
+    
+}
