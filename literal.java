7d88ea3aff2d3fbc285cf443bc4a1ce2e2421804
Ashutosh Mishra <114138083+ashut123@users.noreply.github.com>
2023-05-25 17:52:50 +0530

    Add files via upload

---
+++ literal.java
@@ -0,0 +1,12 @@
+public class literal {
+    public static void main(String arg[])
+    {
+        byte b=12 ;
+        short s=12;
+        int i=12;
+        System.out.println(b);
+    }
+
+    
+}
+ 
