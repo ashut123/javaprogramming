fa4475e4fe698aa23728081ee99ef5ab5deaa046
Ashutosh Mishra <114138083+ashut123@users.noreply.github.com>
2023-05-26 23:04:46 +0530

    Add files via upload

---
+++ literal2.java
@@ -0,0 +1,18 @@
+public class literal2 {
+    public static void main(String arg[])
+    {
+        byte b1=10;
+        byte b2=0b1010;
+        byte b3=012;
+        byte b4=0XA; 
+
+        long l= 125;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+        System.out.println(l);
+    }
+    
+}
