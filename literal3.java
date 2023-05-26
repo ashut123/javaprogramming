679287cafa92bd6f3f8615c904123c2d95953f22
Ashutosh Mishra <114138083+ashut123@users.noreply.github.com>
2023-05-27 01:43:02 +0530

    Add files via upload

---
+++ literal3.java
@@ -0,0 +1,15 @@
+public class literal3 {
+    public static void main(String arg[])
+    {
+         long l=999_999_999L; // in java on the place of comma we use underscore ans we can not use underscore in last
+         //here we can not assign int value L because int is smaller and L is larger
+         float f=12.56f;
+         double d =12.56d;         
+         
+
+         System.out.println(l);
+         System.out.println(f);
+         System.out.println(d);
+    }
+    
+}
