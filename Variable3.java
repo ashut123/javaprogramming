451da36f8ff88072a05fbc1f23592a3367c4c2c4
Ashutosh Mishra <114138083+ashut123@users.noreply.github.com>
2023-06-06 17:55:10 +0530

    Add files via upload

---
+++ Variable3.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+public class Variable3 {
+    public static void main(String arg[])
+    {
+        int x;
+        int X;
+        int roll_number$student;  // here we can not give sapce between roll number
+
+        int _x; // we can not start  from number we can start from _(underscore)
+
+        int String=10;
+        System.out.println(String);
+
+        int rollNumberOfStudent;
+        float averageMarksOfClass;
+
+
+
+
+
+    }
+    
+}
