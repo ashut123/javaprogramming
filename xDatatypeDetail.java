2879455bdd4b2dc86dea0d9c6c8ca9e0544367b3
Ashutosh Mishra <114138083+ashut123@users.noreply.github.com>
2023-06-07 10:23:59 +0530

    Add files via upload

---
+++ xDatatypeDetail.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import javax.swing.*;
+import java.awt.*;
+
+public class xDatatypeDetail {
+    public static void main(String arg[])
+    {
+        int x=5; // convert into binary form  and we can not convert all into binary form and float we can not convert into binary form
+        int y=-5; // for 2's compelement
+        System.out.println(Integer.toBinaryString(x));
+        System.out.println(Integer.toBinaryString(y));
+    }
+    
+}
