0ca266bc761edc130b16f6612ddd70d5ffb26f66
Ashutosh Mishra <114138083+ashut123@users.noreply.github.com>
2023-06-14 00:46:01 +0530

    Add files via upload

---
+++ XMYfirst.java
@@ -0,0 +1,10 @@
+public class XMYfirst {
+    public static void main (String arg[])
+    {
+        char x=0x03c8;  // for symbol we have to use thid code
+
+        
+        System.out.println(x);
+    }
+    
+}
