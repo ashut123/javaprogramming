34bc096499559c0a146083dde46f673b76568c14
Ashutosh Mishra <114138083+ashut123@users.noreply.github.com>
2023-06-23 19:37:48 +0530

    Add files via upload

---
+++ XMyfirst2.java
@@ -0,0 +1,8 @@
+public class XMyfirst2 {
+    public static void main(String arg[])
+    {
+        for(char c=0x0900;c<=0x0970;c++) //for printing all alpha bets
+        System.out.println(c+" ");
+    }
+    
+}
